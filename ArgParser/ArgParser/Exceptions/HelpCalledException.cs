9d0880a825ea270fb7244e37d0287533e2468a2b
Tereza <hoskova.terka@gmail.com>
2026-03-10 15:21:17 +0100

    add helpCalledException

---
+++ ArgParser/ArgParser/Exceptions/HelpCalledException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArgParser.Exceptions;
+
+/// <summary>
+/// The exception that is thrown while parsing command line arguments if help option was given ("--help", "-h").
+/// </summary>
+public sealed class HelpCalledException : Exception
+{
+    internal HelpCalledException(string? message)
+        : base(message) { }
+
+    /// <summary>
+    /// Property that contains the generated help message.
+    /// </summary>
+    public string HelpMessage { get; internal set; } = string.Empty;
+}
