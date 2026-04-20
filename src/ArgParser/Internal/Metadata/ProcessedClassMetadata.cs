06d4cc25bbb72984eb7f59a4c13d006820e42fe0
Tereza <hoskova.terka@gmail.com>
2026-04-20 13:48:01 +0200

    add names to flag

--- src/ArgParser/Internal/Metadata/ProcessedClassMetadata.cs
+++ src/ArgParser/Internal/Metadata/ProcessedClassMetadata.cs
@@ -2,56 +2,61 @@ namespace ArgParser.Internal.Metadata;
 
 internal class ProcessedClassMetadata
 {
-    internal required Dictionary<string, ITerminatingFlag> TerminatingFlags { get; init; }
-    internal required Dictionary<string, PropertyMetadata> LongNamesToProperty { get; init; }
-    internal required Dictionary<string, PropertyMetadata> ShortNamesToProperty { get; init; }
+    internal required Dictionary<string, PropertyMetadata> NamesToFlag { get; init; }
+    internal required Dictionary<string, PropertyMetadata> LongNamesToOption { get; init; }
+    internal required Dictionary<string, PropertyMetadata> ShortNamesToOption { get; init; }
 
     internal ArgsClassMetadata ClassMetadata { get; }
 
     private ProcessedClassMetadata(ArgsClassMetadata classMetadata) =>
         ClassMetadata = classMetadata;
 
-    private static Dictionary<string, ITerminatingFlag> GetTerminatingFlags(
+    private static Dictionary<string, PropertyMetadata> GetNamesToFlag(
         ArgsClassMetadata metadata
     )
     {
-        Dictionary<string, ITerminatingFlag> terminatingFlags = new();
+        Dictionary<string, PropertyMetadata> namesToFlag = new();
         foreach (PropertyMetadata p in metadata.Properties)
         {
-            BehaviorMetadata behavior = p.Behavior;
-            if (behavior.TerminatingFlag is null)
+            if (!p.IsFlag())
                 continue;
-
-            foreach (char shortName in behavior.ShortNames)
-                terminatingFlags.Add($"-{shortName}", behavior.TerminatingFlag);
+            BehaviorMetadata behavior = p.Behavior;
 
             foreach (string longName in behavior.LongNames)
-                terminatingFlags.Add($"--{longName}", behavior.TerminatingFlag);
-        }
+                namesToFlag.Add($"--{longName}", p);
 
-        return terminatingFlags;
+            foreach (char shortName in behavior.ShortNames)
+                namesToFlag.Add($"-{shortName}", p);
+        }
+        return namesToFlag;
     }
 
-    private static Dictionary<string, PropertyMetadata> GetLongNamesToProperty(
+    private static Dictionary<string, PropertyMetadata> GetLongNamesToOption(
         ArgsClassMetadata metadata
     )
     {
         Dictionary<string, PropertyMetadata> longNamesToProperty = new();
         foreach (PropertyMetadata p in metadata.Properties)
         {
+            if(p.IsFlag()) 
+                continue;
+
             foreach (string longName in p.Behavior.LongNames)
                 longNamesToProperty.Add($"--{longName}", p);
         }
         return longNamesToProperty;
     }
 
-    private static Dictionary<string, PropertyMetadata> GetShortNamesToProperty(
+    private static Dictionary<string, PropertyMetadata> GetShortNamesToOption(
         ArgsClassMetadata metadata
     )
     {
         Dictionary<string, PropertyMetadata> shortNamesToProperty = new();
         foreach (PropertyMetadata p in metadata.Properties)
         {
+            if (p.IsFlag())
+                continue;
+
             foreach (char shortName in p.Behavior.ShortNames)
                 shortNamesToProperty.Add($"-{shortName}", p);
         }
@@ -61,8 +66,8 @@ internal class ProcessedClassMetadata
     internal static ProcessedClassMetadata FromMetadata(ArgsClassMetadata metadata) =>
         new(metadata)
         {
-            TerminatingFlags = GetTerminatingFlags(metadata),
-            LongNamesToProperty = GetLongNamesToProperty(metadata),
-            ShortNamesToProperty = GetShortNamesToProperty(metadata),
+            NamesToFlag = GetNamesToFlag(metadata),
+            LongNamesToOption = GetLongNamesToOption(metadata),
+            ShortNamesToOption = GetShortNamesToOption(metadata),
         };
 }
